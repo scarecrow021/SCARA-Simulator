e02e4f93ff7aa0f29097e2f6df94257917ca8ee8
scarecrow021 <soumiksarkar1997@gmail.com>
2021-09-08 19:23:02 +0200

    Kinematics files created

--- SCARA_Simulator/src/UI/mainwindow.cpp
+++ SCARA_Simulator/src/UI/mainwindow.cpp
@@ -1,4 +1,4 @@
-#include "mainwindow.h"
+#include "./UI/mainwindow.h"
 #include "./ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
@@ -20,23 +20,28 @@ MainWindow::~MainWindow()
 
 void MainWindow::DrawSCARA(int junk)
 {
-    std::cout<<junk;
+    
+    
     QGraphicsLineItem *line1;
     QGraphicsLineItem *line2;
 
     QPen redPen(Qt::red);
     QPen blackPen(Qt::black);
 
-    qreal l1 = 100;
-    qreal l2 = 100;
+    redPen.setWidth(5);
+    blackPen.setWidth(5);
 
-    qreal x0 = 0;
-    qreal y0 = 0;
+    double l1 = 100;
+    double l2 = 100;
 
-    qreal x1,x2,y1,y2;
 
-    qreal q1 = ui->q1->value();
-    qreal q2 = ui->q2->value();
+    double x0 = 0;
+    double y0 = 0;
+
+    double x1,x2,y1,y2;
+
+    double q1 = ui->q1->value();
+    double q2 = ui->q2->value();
 
     x1 = l1 * cos(M_PI*q1/180);
     y1 = l1 * sin(M_PI*q1/180);
@@ -44,7 +49,7 @@ void MainWindow::DrawSCARA(int junk)
     x2 = l1 * cos(M_PI*q1/180) + l2 * cos((M_PI*q1/180) + (M_PI*q2/180));
     y2 = l1 * sin(M_PI*q1/180) + l2 * sin((M_PI*q1/180) + (M_PI*q2/180));
 
-    //scene->clear();
+    scene->clear();
     line1 = scene->addLine(x0,y0,x1,y1,redPen);
     line2 = scene->addLine(x1,y1,x2,y2,blackPen);
 
