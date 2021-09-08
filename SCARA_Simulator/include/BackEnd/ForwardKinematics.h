5844eddbc6452cd5917674bd7f84e893b2d1bf2b
scarecrow021 <soumiksarkar1997@gmail.com>
2021-09-08 19:36:02 +0200

    edit

--- SCARA_Simulator/include/BackEnd/ForwardKinematics.h
+++
@@ -1,8 +0,0 @@
-/**
-*	\class		SCARA
-*	\brief		A class to compute the forward kinematics of SCARA robot
-*	\author		Soumik SARKAR (Intern, CEA LIST)
-*	\author		Olivier DAVID (Research Engineer, CEA LIST)
-*	\version	SceneCreator_1.2
-*	\date		26/04/2021
-*/
---
+++ SCARA_Simulator/include/BackEnd/SCARA.h
@@ -0,0 +1,7 @@
+/**
+*	\class		SCARA
+*	\brief		A class to compute the forward kinematics of SCARA robot
+*	\author		Soumik SARKAR
+*	\version	
+*	\date		
+*/
